181eb6eb5ff01320fdf3456bbfad6e2f8b299766
DESKTOP-RG5G4MO\tyler <seesharpcode@gmail.com>
2018-04-16 02:02:22 -0500

    added gulp deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ let gulp = require('gulp'),
     clean = require('gulp-clean'),
     include = require('gulp-include');
 
-gulp.task('clean', function() {
-    return gulp.src('dist', {read: false})
+gulp.task('clean', function () {
+    return gulp.src('dist', { read: false })
         .pipe(clean());
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('build', ['clean'], function () {
     gulp.src('index.js')
         .pipe(include())
         .on('error', console.log)
@@ -16,8 +16,21 @@ gulp.task('build', ['clean'], function() {
     gulp.src('partials/*.html')
         .pipe(gulp.dest('dist/partials'));
 
+    gulp.src('config/*.json')
+        .pipe(gulp.dest('dist/config'));
+
     gulp.src('module.json')
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-deploy', function () {
+    return gulp.src('../zEdit Alpha v0.3.0 - Portable/modules/patchus-maximus', { read: false, force: true })
+        .pipe(clean({force: true}));
+});
+
+gulp.task('deploy', ['clean-deploy'], function() {
+    gulp.src('dist/**')
+        .pipe(gulp.dest('../zEdit Alpha v0.3.0 - Portable/modules/patchus-maximus'));
 });
 
 gulp.task('default', ['build']);
