0adff4730d1132a78fcd706fc589762c121b6d30
DESKTOP-RG5G4MO\tyler <seesharpcode@gmail.com>
2018-04-16 02:04:50 -0500

    updated game-settings.js to use config files

--- patchers/game-settings.js
+++ patchers/game-settings.js
@@ -1,27 +1,18 @@
-const gameSettings = {
-    fArmorScalingFactor: .1,
-    fMaxArmorRating: 90.0,
-    fArmorRatingMax: 1.75,
-    fArmorRatingPCMax: 1.4
-};
-
-let gameSettingsKeys = Object.keys(gameSettings);
-
 // TODO: if useWarrior
 patchers.push({
     load: function (plugin, helpers, settings, locals) {
         return {
             signature: 'GMST',
             filter: function (record) {
-                let editorId = xelib.EditorID(record);
-                return gameSettingsKeys.indexOf(editorId) > -1;
+                let editorID = xelib.EditorID(record);
+                return Object.keys(locals.gameSettings).indexOf(editorID) > -1;
             }
         }
     },
     patch: function (record, helpers, settings, locals) {
-        let editorId = xelib.EditorID(record);
-        let value = gameSettings[editorId];
-        helpers.logMessage(`Setting ${editorId} to ${value}`);
+        let editorID = xelib.EditorID(record);
+        let value = locals.gameSettings[editorID];
+        helpers.logMessage(`Setting ${editorID} to ${value}`);
         xelib.SetFloatValue(record, 'DATA\\Float', value);
     }
 });
