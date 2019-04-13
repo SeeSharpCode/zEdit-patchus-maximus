437582565966f21addbdbe2f8e73c6587028b93c
Tyler Daniels <seesharpcode@gmail.com>
2019-04-13 00:16:00 -0500

    all that time and I'm committing this?

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -1,8 +1,15 @@
-export default function($scope, skyrimMaterialService, skyrimArmorKeywordService) {
+export default function($scope, skyrimMaterialService) {
+  const isArmor = armorType => armorType !== 'Clothing' && armorType !== 'Jewelry';
+  const isDummy = armor => xelib.HasKeyword(armor, 'Dummy');
+
+  const isPlayable = armor => !(xelib.HasElement(armor, 'BODT\\General Flags') && xelib.GetFlag(armor, 'BODT\\General Flags', '(ARMO)Non-Playable'))
+    && !xelib.GetRecordFlag(armor, 'Non-Playable');
+
+  // TODO ArmorDarkBrotherhood, etc. keywords
   const armorWithoutMaterials = xelib.GetRecords(0, 'ARMO')
-    .filter(armor => skyrimArmorKeywordService.getArmorType(armor) !== 'Clothing' && skyrimArmorKeywordService.getArmorType(armor) !== 'Jewelry')
-    .filter(armor => !skyrimMaterialService.getMaterial(armor))
-    .map(armor => `${xelib.EditorID(armor)} ${skyrimArmorKeywordService.getArmorPart(armor)} ${skyrimArmorKeywordService.getArmorType(armor)}`);
+    .map(armor => xelib.GetWinningOverride(armor))
+    .filter(armor => isArmor(armor) && isPlayable(armor) && !skyrimMaterialService.getMaterial(armor) && !isDummy(armor))
+    .map(armor => `${xelib.FullName(armor)} (${xelib.EditorID(armor)})`);
 
   $scope.armorWithoutMaterials = [...new Set(armorWithoutMaterials)];
 }
