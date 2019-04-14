c204f3cc32aa1781f6e8a59d96118bf2e63d10f2
Tyler Daniels <seesharpcode@gmail.com>
2019-04-14 00:01:22 -0500

    more progress on settings menu

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -1,6 +1,13 @@
+import materials from '../../config/materials.json';
+
 export default function($scope, skyrimMaterialService) {
+  const FACTION_ARMOR_KEYWORDS = ['ArmorMaterialThievesGuild', 'ArmorDarkBrotherhood', 'ArmorNightingale'];
+
   const isArmor = armorType => armorType !== 'Clothing' && armorType !== 'Jewelry';
-  const isDummy = armor => xelib.HasKeyword(armor, 'Dummy');
+  const isDummyArmor = armor => xelib.HasKeyword(armor, 'Dummy');
+
+  const isFactionArmor = armor => FACTION_ARMOR_KEYWORDS.some(keyword => xelib.HasKeyword(armor, keyword))
+    || xelib.FullName(armor).includes('Guard');
 
   const isPlayable = armor => !(xelib.HasElement(armor, 'BODT\\General Flags') && xelib.GetFlag(armor, 'BODT\\General Flags', '(ARMO)Non-Playable'))
     && !xelib.GetRecordFlag(armor, 'Non-Playable');
@@ -8,8 +15,10 @@ export default function($scope, skyrimMaterialService) {
   // TODO ArmorDarkBrotherhood, etc. keywords
   const armorWithoutMaterials = xelib.GetRecords(0, 'ARMO')
     .map(armor => xelib.GetWinningOverride(armor))
-    .filter(armor => isArmor(armor) && isPlayable(armor) && !skyrimMaterialService.getMaterial(armor) && !isDummy(armor))
+    .filter(armor => isArmor(xelib.GetArmorType(armor)) && isPlayable(armor) && !skyrimMaterialService.getMaterial(armor)
+      && !isDummyArmor(armor) && !isFactionArmor(armor))
     .map(armor => `${xelib.FullName(armor)} (${xelib.EditorID(armor)})`);
 
+  $scope.materials = Object.keys(materials);
   $scope.armorWithoutMaterials = [...new Set(armorWithoutMaterials)];
 }
